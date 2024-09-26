ff2fec4d065f70ce69f8428fe5441d97b9390378
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2024-09-26 20:12:53 -0300

    log in section

--- src/pages/login/components/login-form.jsx
+++ src/pages/login/components/login-form.jsx
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-
+import { login } from "../services/login.service";
+import { message } from "antd";
 export function LoginForm() {
   const nav = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+    const data = {
+      email: email,
+      password: password,
+    };
     try {
+      if (data.email === "" || data.password === "") {
+        message.info("Faltan datos.");
+        return;
+      }
+      setIsLoading(true);
+      const response = await login(data);
+      console.log("respuesta del Login", response);
+
       nav("/home");
     } catch (error) {
+      message.error("Credenciales invalidas");
       console.log("error at login", error);
+    } finally {
+      setIsLoading(false);
     }
   };
+
   return (
     <form
       className=" w-full flex flex-col gap-4 items-center"
@@ -22,17 +42,26 @@ export function LoginForm() {
           placeholder="user@example.com"
           type="text"
           className="border-none outline-none rounded-sm p-2"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
         <input
           placeholder="*****"
           type="password"
           className="border-none outline-none rounded-sm p-2"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
       </div>
 
       <div className="flex flex-col gap-4 items-center">
-        <button className="bg-white px-4 py-1 rounded-md font-semibold text-blue-500">
-          Log in
+        <button
+          className={`bg-white px-4 py-1 rounded-md font-semibold text-blue-500 ${
+            isLoading ? "opacity-40" : ""
+          }`}
+          disabled={isLoading}
+        >
+          {isLoading ? "cargando.." : "Log in"}
         </button>
         <p className="text-white">
           You dont have account?{" "}
